c9ab7cbda60d525a44c67c56278eb33f05fab243
Abhishek-0809 <abhishekwaghmare34@gmail.com>
2023-07-10 00:40:26 +0530

    Enhanced colors for better UI experience.

--- tracker.py
+++ tracker.py
@@ -14,7 +14,7 @@ root = Tk()
 root.title("Phone Number Tracker")
 root.geometry("365x584")
 root.resizable(False, False)
-root.configure(bg="#d896ff")
+root.configure(bg="#363062")
 # background color code is #d896ff
 # individual background color code is #d8b9ff
 # san francisco number +14155553890
@@ -54,41 +54,41 @@ def track():
     current_time = local_time.strftime("%I : %M %p")
     clock.config(text=current_time)
 
-Heading = Label(root, text="TRACK NUMBER", bg="#d8b9ff",
+Heading = Label(root, text="TRACK NUMBER",fg="white", bg="#363062",
                 font=("arial", 25, "bold"), justify="center")
 Heading.place(x=43, y=110)
 
 entry = StringVar()
 enter_number = Entry(root, textvariable=entry, width=19, bd=0,
-                     background="#d8b9ff", font=("arial", 15), justify="center")
+                     background="#827397", font=("arial", 15), justify="center")
 enter_number.place(x=75, y=200)
 
-search = Button(text="SEARCH", width=18, bd=1, bg="#d8b9ff",
+search = Button(text="SEARCH", width=18, bd=1, fg="white", bg="#827397",
                 font=("arial", 15), justify="center", command=track)
 search.place(x=75, y=300)
 
-country = Label(root, text="COUNTRY:", bg="#d8b9ff",
-                fg="black", font=("arial", 10, "bold"))
+country = Label(root, text="COUNTRY:", bg="#363062",
+                fg="white", font=("arial", 10, "bold"))
 country.place(x=50, y=400)
 
-sim = Label(root, text="SIM:", bg="#d8b9ff",
-            fg="black", font=("arial", 10, "bold"))
+sim = Label(root, text="SIM:", bg="#363062",
+            fg="white", font=("arial", 10, "bold"))
 sim.place(x=200, y=400)
 
-zone = Label(root, text="TIME ZONE:", bg="#d8b9ff",
-             fg="black", font=("arial", 10, "bold"))
+zone = Label(root, text="TIME ZONE:", bg="#363062",
+             fg="white", font=("arial", 10, "bold"))
 zone.place(x=50, y=450)
 
-clock = Label(root, text="PHONE TIME:", bg="#d8b9ff",
-              fg="black", font=("arial", 10, "bold"))
+clock = Label(root, text="PHONE TIME:", bg="#363062",
+              fg="white", font=("arial", 10, "bold"))
 clock.place(x=200, y=450)
 
-longitude = Label(root, text="LONGITUDE:", bg="#d8b9ff",
-                  fg="black", font=("arial", 10, "bold"))
+longitude = Label(root, text="LONGITUDE:", bg="#363062",
+                  fg="white", font=("arial", 10, "bold"))
 longitude.place(x=50, y=500)
 
-latitude = Label(root, text="LATITUDE:", bg="#d8b9ff",
-                 fg="black", font=("arial", 10, "bold"))
+latitude = Label(root, text="LATITUDE:", bg="#363062",
+                 fg="white", font=("arial", 10, "bold"))
 latitude.place(x=200, y=500)
 
 root.mainloop()
